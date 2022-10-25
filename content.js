0708ecd90a39b72f81596b2422a7dbe9c352fd8b
gmphato <giftmphato@gmail.com>
2022-10-25 18:41:27 +0000

    updates

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 const divs = document.querySelectorAll('div');
 
 divs.forEach(d => {
-    d.style.border = '2px dotted #2a9d8f'
+    d.style.border = '1px dotted #2a9d8f'
     const node = createItem('DIV')
     d.appendChild(node)
 });
@@ -11,5 +11,7 @@ function createItem(name) {
     pre.textContent = name;
     pre.style.fontSize = '12px'
     pre.style.border = '1px dotted red';
+    pre.style.position = 'absolute',
+    pre.style.zIndex = 99999;
     return pre;
 }
