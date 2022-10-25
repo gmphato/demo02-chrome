1f2feefdc0bc82a0795ff7fa9d302ef2cca81d8d
gmphato <giftmphato@gmail.com>
2022-10-25 18:37:29 +0000

    add child node

--- content.js
+++ content.js
@@ -1,4 +1,15 @@
-var images = document.getElementsByTagName('img');
-for (var i = 0, l = images.length; i < l; i++) {
-  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+const divs = document.querySelectorAll('div');
+
+divs.forEach(d => {
+    d.style.border = '2px dotted #2a9d8f'
+    const node = createItem('DIV')
+    d.appendChild(node)
+});
+
+function createItem(name) {
+    let pre = document.createElement('prev');
+    pre.textContent = name;
+    pre.style.fontSize = '12px'
+    pre.style.border = '1px dotted red';
+    return pre;
 }
